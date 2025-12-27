9cd137dca4fa73965fa71ad292146813b52ce3ee
Procode9 <36112329+Procode9@users.noreply.github.com>
2025-12-27 20:20:07 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/Login";
@@ -6,34 +5,15 @@ import Register from "./pages/Register";
 import JobList from "./pages/JobList";
 import JobDetail from "./pages/JobDetail";
 
-function isAuthenticated() {
-  return !!localStorage.getItem("token");
-}
-
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Root */}
-        <Route
-          path="/"
-          element={
-            isAuthenticated()
-              ? <Navigate to="/jobs" replace />
-              : <Navigate to="/login" replace />
-          }
-        />
-
-        {/* Auth */}
+        <Route path="/" element={<Navigate to="/jobs" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
-        {/* Jobs */}
         <Route path="/jobs" element={<JobList />} />
         <Route path="/jobs/:jobId" element={<JobDetail />} />
-
-        {/* Fallback */}
-        <Route path="*" element={<div style={{ padding: 24 }}>404 – Page not found</div>} />
       </Routes>
     </BrowserRouter>
   );
