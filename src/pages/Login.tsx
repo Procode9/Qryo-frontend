0786ff385680eff83347c1f1587060a89662092b
Procode9 <36112329+Procode9@users.noreply.github.com>
2025-12-27 18:54:28 +0300

    Create Login.tsx

---
+++ src/pages/Login.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { login } from "../auth/auth.service";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const nav = useNavigate();
+
+  async function submit() {
+    await login(email, password);
+    nav("/jobs");
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <input placeholder="email" onChange={e => setEmail(e.target.value)} />
+      <input placeholder="password" type="password" onChange={e => setPassword(e.target.value)} />
+      <button onClick={submit}>Login</button>
+    </div>
+  );
+}
