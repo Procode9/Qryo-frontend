0fa8ee398fb8a9bdb519d1576f01fe7a531ce436
Procode9 <36112329+Procode9@users.noreply.github.com>
2025-12-27 18:53:51 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RequireAuth } from "./auth/auth.guard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Jobs from "./pages/Jobs";
+import JobDetail from "./pages/JobDetail";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/jobs"
+          element={
+            <RequireAuth>
+              <Jobs />
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/jobs/:id"
+          element={
+            <RequireAuth>
+              <JobDetail />
+            </RequireAuth>
+          }
+        />
+
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
