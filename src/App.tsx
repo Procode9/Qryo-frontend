4d670f69fb67a1f59707ef5690068bc2a1056d40
Procode9 <36112329+Procode9@users.noreply.github.com>
2025-12-27 20:44:19 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Layout from "./components/Layout";
+
+import Dashboard from "./pages/Dashboard";
 import JobList from "./pages/JobList";
 import JobDetail from "./pages/JobDetail";
 import Login from "./pages/Login";
@@ -10,13 +13,22 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* Public routes (no layout if you want later ayırabiliriz) */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Protected / Platform routes */}
         <Route element={<Layout />}>
-          <Route path="/" element={<JobList />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+
+          <Route path="/jobs" element={<JobList />} />
           <Route path="/jobs/:id" element={<JobDetail />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+
           <Route path="*" element={<NotFound />} />
         </Route>
+
       </Routes>
     </BrowserRouter>
   );
